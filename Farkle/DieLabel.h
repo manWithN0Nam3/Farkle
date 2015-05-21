4db9f9dfea96be2ff840c616395b4f535429cedd
Alex <alexsantorineos@gmail.com>
2015-05-21 14:33:33 -0500

    push

---
+++ Farkle/DieLabel.h
@@ -0,0 +1,13 @@
+//
+//  DieLabel.h
+//  Farkle
+//
+//  Created by Alex Santorineos on 5/21/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DieLabel : NSObject
+
+@end
---
+++ Farkle/DieLabel.m
@@ -0,0 +1,13 @@
+//
+//  DieLabel.m
+//  Farkle
+//
+//  Created by Alex Santorineos on 5/21/15.
+//  Copyright (c) 2015 madApperz. All rights reserved.
+//
+
+#import "DieLabel.h"
+
+@implementation DieLabel
+
+@end
